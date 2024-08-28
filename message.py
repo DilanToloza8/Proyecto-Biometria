4eefd631e3d93931505dac3d7e3e88d9f5c5d1d4
DilanToloza8 <dilan.toloza2003@gmail.com>
2024-08-27 23:50:34 -0400

    Version 1.2

--- message.py
+++ message.py
@@ -1,29 +1,24 @@
-from tkinter import *
+from tkinter import Toplevel, StringVar, Label, Entry, Button, CENTER, TOP
 from tkinter import messagebox as msg
 import os
 import cv2
 from matplotlib import pyplot as plt
-from mtcnn.mtcnn import MTCNN
+from mtcnn import MTCNN
 import database as db
 
 # CONFIG
-path = "C:/Users/Dilan/Desktop/Trabajo/Usuarios" # your path
-
-res_bd = {"id": 0, "affected": 0} # database variable
+path = "C:/Users/Dilan/Desktop/Trabajo/Usuarios"
+detector = MTCNN()
 
 # GENERAL
-def getEnter(screen):
-    ''' Set an enter inside the screen '''
-    Label(screen, text="", bg=color_background).pack()
-
-def printAndShow(screen, text, flag):
+def print_and_show(screen, text, success):
     ''' Prints and shows text '''
-    if flag:
-        print(color_success + text + color_normal)
+    color = color_success if success else color_error
+    print(f"{color}{text}{color_normal}")
+    if success:
         screen.destroy()
         msg.showinfo(message=text, title="¡Éxito!")
     else:
-        print(color_error + text + color_normal)
         Label(screen, text=text, fg="red", bg=color_background, font=(font_label, 12)).pack()
 
 def configure_screen(screen, text):
@@ -33,146 +28,87 @@ def configure_screen(screen, text):
     screen.configure(bg=color_background)
     Label(screen, text=f"¡{text}!", fg=color_white, bg=color_black, font=(font_label, 18), width="500", height="2").pack()
 
-def credentials(screen, var, flag):
+def setup_credentials(screen, var, capture_command):
     ''' Configuration of user input '''
     Label(screen, text="Usuario:", fg=color_white, bg=color_background, font=(font_label, 12)).pack()
     entry = Entry(screen, textvariable=var, justify=CENTER, font=(font_label, 12))
     entry.focus_force()
     entry.pack(side=TOP, ipadx=30, ipady=6)
-
-    getEnter(screen)
-    if flag:
-        Button(screen, text="Capturar rostro", fg=color_white, bg=color_black_btn, activebackground=color_background, borderwidth=0, font=(font_label, 14), height="2", width="40", command=login_capture).pack()
-    else:
-        Button(screen, text="Capturar rostro", fg=color_white, bg=color_black_btn, activebackground=color_background, borderwidth=0, font=(font_label, 14), height="2", width="40", command=register_capture).pack()
+    Button(screen, text="Capturar rostro", fg=color_white, bg=color_black_btn, activebackground=color_background, borderwidth=0, font=(font_label, 14), height="2", width="40", command=capture_command).pack()
     return entry
 
-def face(img, faces):
-    data = plt.imread(img)
-    for i in range(len(faces)):
-        x1, y1, ancho, alto = faces[i]["box"]
-        x2, y2 = x1 + ancho, y1 + alto
-        plt.subplot(1,len(faces), i + 1)
-        plt.axis("off")
-        face = cv2.resize(data[y1:y2, x1:x2],(150,200), interpolation=cv2.INTER_CUBIC)
-        cv2.imwrite(img, face)
-        plt.imshow(data[y1:y2, x1:x2])
+def process_and_save_face(img_path, faces):
+    ''' Process and save detected face '''
+    img_data = plt.imread(img_path)
+    for face in faces:
+        x1, y1, ancho, alto = face["box"]
+        cropped_face = cv2.resize(img_data[y1:y1+alto, x1:x1+ancho], (150, 200), interpolation=cv2.INTER_CUBIC)
+        cv2.imwrite(img_path, cropped_face)
 
 # REGISTER #
-def register_face_db(img):
-    name_user = img.replace(".jpg","").replace(".png","")
-    res_bd = db.registerUser(name_user, path + img)
-
-    getEnter(screen1)
-    if(res_bd["affected"]):
-        printAndShow(screen1, "¡Éxito! Se ha registrado correctamente", 1)
-    else:
-        printAndShow(screen1, "¡Error! No se ha registrado correctamente", 0)
+def register_user_in_db(img):
+    ''' Register the user in the database '''
+    username = os.path.splitext(img)[0]
+    res_bd = db.registerUser(username, os.path.join(path, img))
+    print_and_show(screen1, "¡Éxito! Se ha registrado correctamente" if res_bd["affected"] else "¡Error! No se ha registrado correctamente", res_bd["affected"])
     os.remove(img)
 
-def register_capture():
-    cap = cv2.VideoCapture(0)
-    user_reg_img = user1.get()
-    img = f"{user_reg_img}.jpg"
-
-    while True:
-        ret, frame = cap.read()
-        cv2.imshow("Registro Facial", frame)
-        if cv2.waitKey(1) == 27:
-            break
-    
-    cv2.imwrite(img, frame)
-    cap.release()
-    cv2.destroyAllWindows()
-
+def capture_and_register():
+    ''' Capture image and register face '''
+    img = capture_image(user1.get())
+    process_and_save_face(img, detector.detect_faces(plt.imread(img)))
+    register_user_in_db(img)
     user_entry1.delete(0, END)
-    
-    pixels = plt.imread(img)
-    faces = MTCNN().detect_faces(pixels)
-    face(img, faces)
-    register_face_db(img)
 
 def register():
-    global user1
-    global user_entry1
-    global screen1
-
+    ''' Initialize registration screen '''
+    global user1, user_entry1, screen1
     screen1 = Toplevel(root)
     user1 = StringVar()
-
     configure_screen(screen1, txt_register)
-    user_entry1 = credentials(screen1, user1, 0)
+    user_entry1 = setup_credentials(screen1, user1, capture_and_register)
 
 # LOGIN #
-def compatibility(img1, img2):
+def compare_faces(img1, img2):
+    ''' Compare two faces and return similarity score '''
     orb = cv2.ORB_create()
-
-    kpa, dac1 = orb.detectAndCompute(img1, None)
-    kpa, dac2 = orb.detectAndCompute(img2, None)
-
-    comp = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-
-    matches = comp.match(dac1, dac2)
-
-    similar = [x for x in matches if x.distance < 70]
-    if len(matches) == 0:
+    kp1, desc1 = orb.detectAndCompute(img1, None)
+    kp2, desc2 = orb.detectAndCompute(img2, None)
+    if desc1 is None or desc2 is None:
         return 0
-    return len(similar)/len(matches)
+    matches = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True).match(desc1, desc2)
+    return len([m for m in matches if m.distance < 70]) / len(matches) if matches else 0
 
-def login_capture():
-    cap = cv2.VideoCapture(0)
-    user_login = user2.get()
-    img = f"{user_login}_login.jpg"
-    img_user = f"{user_login}.jpg"
+def capture_and_login():
+    ''' Capture image and attempt login '''
+    img = capture_image(f"{user2.get()}_login")
+    process_and_save_face(img, detector.detect_faces(plt.imread(img)))
 
-    while True:
-        ret, frame = cap.read()
-        cv2.imshow("Login Facial", frame)
-        if cv2.waitKey(1) == 27:
-            break
-    
-    cv2.imwrite(img, frame)
-    cap.release()
-    cv2.destroyAllWindows()
-
-    user_entry2.delete(0, END)
-    
-    pixels = plt.imread(img)
-    faces = MTCNN().detect_faces(pixels)
-
-    face(img, faces)
-    getEnter(screen2)
-
-    res_db = db.getUser(user_login, path + img_user)
-    if(res_db["affected"]):
-        my_files = os.listdir()
-        if img_user in my_files:
-            face_reg = cv2.imread(img_user, 0)
-            face_log = cv2.imread(img, 0)
-
-            comp = compatibility(face_reg, face_log)
-            
-            if comp >= 0.94:
-                print("{}Compatibilidad del {:.1%}{}".format(color_success, float(comp), color_normal))
-                printAndShow(screen2, f"Bienvenido, {user_login}", 1)
-            else:
-                print("{}Compatibilidad del {:.1%}{}".format(color_error, float(comp), color_normal))
-                printAndShow(screen2, "¡Error! Incopatibilidad de datos", 0)
-            os.remove(img_user)
-    
-        else:
-            printAndShow(screen2, "¡Error! Usuario no encontrado", 0)
+    user_img_path = os.path.join(path, f"{user2.get()}.jpg")
+    if db.getUser(user2.get(), user_img_path)["affected"] and os.path.exists(user_img_path):
+        comp = compare_faces(cv2.imread(user_img_path, 0), cv2.imread(img, 0))
+        login_success = comp >= 0.94
+        print_and_show(screen2, f"Bienvenido, {user2.get()}" if login_success else "¡Error! Incompatibilidad de datos", login_success)
+        os.remove(user_img_path)
     else:
-        printAndShow(screen2, "¡Error! Usuario no encontrado", 0)
+        print_and_show(screen2, "¡Error! Usuario no encontrado", 0)
     os.remove(img)
+    user_entry2.delete(0, END)
 
 def login():
-    global screen2
-    global user2
-    global user_entry2
-
+    ''' Initialize login screen '''
+    global screen2, user2, user_entry2
     screen2 = Toplevel(root)
     user2 = StringVar()
-
     configure_screen(screen2, txt_login)
-    user_entry2 = credentials(screen2, user2, 1)
+    user_entry2 = setup_credentials(screen2, user2, capture_and_login)
+
+def capture_image(filename):
+    ''' Capture an image using the webcam '''
+    cap = cv2.VideoCapture(0)
+    ret, frame = cap.read()
+    img_path = f"{filename}.jpg"
+    cv2.imwrite(img_path, frame)
+    cap.release()
+    cv2.destroyAllWindows()
+    return img_path
